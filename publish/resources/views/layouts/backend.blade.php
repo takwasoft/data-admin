6a69156abd83d31e01fa98905bf9c796613e9bb2
ashikur37 <piash3700@gmail.com>
2020-10-30 18:22:17 +0600

    'datatable'

--- publish/resources/views/layouts/backend.blade.php
+++ publish/resources/views/layouts/backend.blade.php
@@ -88,6 +88,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.8.1/tinymce.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript">
         tinymce.init({
             selector: '.crud-richtext'
